d4b7d16d5678668bf7b88a7f4f2845c46bf3c36c
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-21 21:47:23 +0700

    add
    bai tap

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        // Bước 1: Khai báo và khởi động mảng số nguyên
+
         Console.Write("Nhập số lượng phần tử N: ");
         int N = Convert.ToInt32(Console.ReadLine());
 
@@ -16,11 +16,9 @@ class Program
             array[i] = Convert.ToInt32(Console.ReadLine());
         }
 
-        // Bước 2: Nhập X là phần tử cần xóa
         Console.Write("Nhập phần tử cần xóa X: ");
         int X = Convert.ToInt32(Console.ReadLine());
 
-        // Bước 3: Tìm X trong mảng
         int index_del = -1;
         for (int i = 0; i < N; i++)
         {
@@ -31,16 +29,15 @@ class Program
             }
         }
 
-        // Bước 4: Nếu tìm thấy, thực hiện xóa
         if (index_del != -1)
         {
             for (int i = index_del; i < N - 1; i++)
             {
-                array[i] = array[i + 1]; // dịch trái
+                array[i] = array[i + 1]; 
             }
-            N--; // giảm kích thước hợp lệ của mảng
+            N--;
 
-            // Bước 5: In ra mảng sau khi xóa
+            
             Console.WriteLine($"Mảng sau khi xóa phần tử {X}:");
             for (int i = 0; i < N; i++)
             {
